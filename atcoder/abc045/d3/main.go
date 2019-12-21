26f4fcd80f25fd58405a9451c05dba0cc53f7096
YAEGASHI Takeshi <yaegashi@gmail.com>
2019-12-21 19:09:13 +0900

    Update atcoder/abc045/d3

--- atcoder/abc045/d3/main.go
+++ atcoder/abc045/d3/main.go
@@ -1,16 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type contest struct {
+	in  io.Reader
+	out io.Writer
+}
+
+func (con *contest) scan(a ...interface{}) (int, error) {
+	return fmt.Fscan(con.in, a...)
+}
+func (con *contest) scanln(a ...interface{}) (int, error) {
+	return fmt.Fscanln(con.in, a...)
+}
+func (con *contest) scanf(f string, a ...interface{}) (int, error) {
+	return fmt.Fscanf(con.in, f, a...)
+}
+func (con *contest) print(a ...interface{}) (int, error) {
+	return fmt.Fprint(con.out, a...)
+}
+func (con *contest) println(a ...interface{}) (int, error) {
+	return fmt.Fprintln(con.out, a...)
+}
+func (con *contest) printf(f string, a ...interface{}) (int, error) {
+	return fmt.Fprintf(con.out, f, a...)
+}
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	con := &contest{in: in, out: out}
+	con.main()
+}
 
 type p struct{ r, c int }
 
-func main() {
+func (con *contest) main() error {
 	var H, W, N int
-	fmt.Scan(&H, &W, &N)
+	con.scan(&H, &W, &N)
 	M := map[p]int{}
 	for i := 0; i < N; i++ {
 		var a, b int
-		fmt.Scan(&a, &b)
+		con.scan(&a, &b)
 		for r := a - 3; r < a; r++ {
 			if r < 0 || r > H-3 {
 				continue
@@ -29,6 +65,7 @@ func main() {
 	}
 	A[0] = (H-2)*(W-2) - len(M)
 	for _, a := range A {
-		fmt.Println(a)
+		con.println(a)
 	}
+	return nil
 }
