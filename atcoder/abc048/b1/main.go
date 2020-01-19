4913b34719d5623e169e94ee12d85235281ab090
YAEGASHI Takeshi <yaegashi@gmail.com>
2020-01-19 21:49:00 +0900

    Add atcoder/abc048/b1

---
+++ atcoder/abc048/b1/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type contest struct {
+	in  io.Reader
+	out io.Writer
+}
+
+func (con *contest) Scan(a ...interface{}) (int, error) {
+	return fmt.Fscan(con.in, a...)
+}
+func (con *contest) Scanln(a ...interface{}) (int, error) {
+	return fmt.Fscanln(con.in, a...)
+}
+func (con *contest) Scanf(f string, a ...interface{}) (int, error) {
+	return fmt.Fscanf(con.in, f, a...)
+}
+func (con *contest) Print(a ...interface{}) (int, error) {
+	return fmt.Fprint(con.out, a...)
+}
+func (con *contest) Println(a ...interface{}) (int, error) {
+	return fmt.Fprintln(con.out, a...)
+}
+func (con *contest) Printf(f string, a ...interface{}) (int, error) {
+	return fmt.Fprintf(con.out, f, a...)
+}
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	con := &contest{in: in, out: out}
+	con.main()
+}
+
+func (con *contest) main() error {
+	var a, b, x int
+	con.Scan(&a, &b, &x)
+	p := 0
+	if a > 0 {
+		p = (a-1)/x + 1
+	}
+	q := b/x + 1
+	con.Println(q - p)
+	return nil
+}
