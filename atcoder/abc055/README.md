544db1d5574a06594a0c444b76f9d5ea6303ff59
YAEGASHI Takeshi <yaegashi@gmail.com>
2020-03-06 04:58:09 +0900

    Add atcoder/abc055/a1 atcoder/abc055/b1 atcoder/abc055/c1

---
+++ atcoder/abc055/README.md
@@ -0,0 +1,15 @@
+# ABC055
+
+https://atcoder.jp/contests/abc055
+
+## A
+
+- https://atcoder.jp/contests/abc055/submissions/10553556
+
+## B
+
+- https://atcoder.jp/contests/abc055/submissions/10553564
+
+## C
+
+- https://atcoder.jp/contests/abc055/submissions/10553599
---
+++ atcoder/abc055/c1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+type contest struct {
+	in  io.Reader
+	out io.Writer
+}
+
+func (con *contest) Scan(a ...interface{}) (int, error) {
+	return fmt.Fscan(con.in, a...)
+}
+func (con *contest) Scanln(a ...interface{}) (int, error) {
+	return fmt.Fscanln(con.in, a...)
+}
+func (con *contest) Scanf(f string, a ...interface{}) (int, error) {
+	return fmt.Fscanf(con.in, f, a...)
+}
+func (con *contest) Print(a ...interface{}) (int, error) {
+	return fmt.Fprint(con.out, a...)
+}
+func (con *contest) Println(a ...interface{}) (int, error) {
+	return fmt.Fprintln(con.out, a...)
+}
+func (con *contest) Printf(f string, a ...interface{}) (int, error) {
+	return fmt.Fprintf(con.out, f, a...)
+}
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	con := &contest{in: in, out: out}
+	con.main()
+}
+
+func (con *contest) main() error {
+	var N, M int
+	con.Scan(&N, &M)
+	A := 0
+	if N > M/2 {
+		A = M / 2
+	} else {
+		M -= N * 2
+		A = N + M/4
+	}
+	con.Println(A)
+	return nil
+}
